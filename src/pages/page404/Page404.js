238acbb77429f6898d1550a5131b371cc144afa1
Damian Zmuda <d.zmuda94@gmail.com>
2020-09-30 15:40:59 +0200

    Add Page404 scaffolding

---
+++ src/pages/page404/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page404 = () => {
+    return (
+        <div className="page-404">
+            <h2>Page not found 404</h2>
+        </div>
+    );
+}
+
+export default Page404;
